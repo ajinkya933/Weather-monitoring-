64d9b0054bb35e645f15f0309a4b95b82216613c
ajinkya bobade <ajinkyabobade@ajinkyas-MacBook-Air.local>
2017-04-26 22:59:30 -0400

    making appear appear on the same line

--- weather.py
+++ weather.py
@@ -3,9 +3,7 @@ email = []
 email_file = open('emails.txt', 'r')
 
 for line in email_file:
-    
-#    (email, name) = line.split(',')
 
+    email.append(line.strip())      # removing the new line between two emails
 
-    email.append(line)
 print(email)
