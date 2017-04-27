7c8c86c63983510c63f3324a04404154b72155c7
ajinkya bobade <ajinkyabobade@ajinkyas-MacBook-Air.local>
2017-04-26 22:50:23 -0400

    display list of emails

--- weather.py
+++ weather.py
@@ -3,6 +3,9 @@ email = []
 email_file = open('emails.txt', 'r')
 
 for line in email_file:
-    (email, name) = line.split(',')
+    
+#    (email, name) = line.split(',')
 
+
+    email.append(line)
 print(email)
